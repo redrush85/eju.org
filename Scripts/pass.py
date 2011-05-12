1ea320e13b082a759b1a013f133860deecd9769a
RedRush <redrush85@gmail.com>
2011-05-12 19:04:33 +0300

    second commit

---
+++ Scripts/pass.py
@@ -0,0 +1,11 @@
+def passgen(size=None):
+  import random
+  import string
+
+  if not size:
+    size = 8
+  return ''.join(random.choice(string.letters + string.digits) for i in range(size))
+  
+  
+print passgen()
+input()
